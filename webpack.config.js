72e33580dff7f06c5f4e5573132caa3ffe6853c5
manuelmelchiorri96 <manuelmelchiorri1996@gmail.com>
2024-01-07 12:11:25 +0100

    aggiornamenti percorsi

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,8 @@ const stylesHandler = isProduction
 const config = {
   entry: "./src/index.js",
   output: {
+    filename: "index.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/",
   },
 
   devServer: {
